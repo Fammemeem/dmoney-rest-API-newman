f8ca2835d685dc22e0d71d8d4ba826ddbf6a058d
Famme Akter Meem <106922643+Fammemeem@users.noreply.github.com>
2023-11-30 19:38:34 +0600

    Add files via upload

---
+++ report.js
@@ -0,0 +1,18 @@
+const newman = require('newman');
+require('dotenv').config();
+ 
+newman.run({
+    //collection: require('./collection/dmoney.json'),
+    collection:`https://api.postman.com/collections/31248032-fced09f1-87c3-4d4f-bd46-67aacf8fa120?access_key=${process.env.accessKey}`,
+    //environment:require('./collection/env.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
